51157e884bc05ace8eccf432e77377aa39b2c2de
lgalvemu-arch <l.galvemu@gmail.com>
2026-03-31 10:07:31 +0200

    added to gitignore

--- WebApplication/obj/Debug/net10.0/WebApplication.AssemblyInfo.cs
+++ WebApplication/obj/Debug/net10.0/WebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a72d21f89c8f3d10480b38361a93e8ce4e91045")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd7f778bb855c6b08c90887c243363573d3206f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
